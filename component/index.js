e5fe9af5edc886507ba0631847ad692419a5ee75
sirbeagle <cbenjamin@stormseed.com>
2019-06-02 17:16:55 -0400

    ugh, trying to get NPM to have the correct files in the archive

--- component/index.js
+++ component/index.js
@@ -1,4 +1,4 @@
-export {
+import {
   CalendarEventMixin,
   CalendarMixin,
   CalendarParentComponentMixin,
@@ -19,7 +19,7 @@ export {
   CalendarTimeLabelTemplateMixin
 } from './calendar/mixins'
 
-export {
+import {
   CalendarAgendaInner,
   CalendarAgendaEvent,
   CalendarAllDayEvents,
@@ -30,3 +30,35 @@ export {
   CalendarMultiDayContent,
   CalendarTimeLabelColumn
 } from './calendar/templates/common'
+
+export {
+  // mixins
+  CalendarEventMixin,
+  CalendarMixin,
+  CalendarParentComponentMixin,
+  EventPropsMixin,
+  CalendarTemplateMixin,
+  CalendarAgendaTemplateMixin,
+  CalendarAgendaEventTemplateMixin,
+  CalendarAllDayEventsTemplateMixin,
+  CalendarDayColumnTemplateMixin,
+  CalendarDayLabelsTemplateMixin,
+  CalendarEventTemplateMixin,
+  CalendarEventDetailTemplateMixin,
+  CalendarHeaderNavTemplateMixin,
+  CalendarMonthTemplateMixin,
+  CalendarMonthInnerTemplateMixin,
+  CalendarMultiDayTemplateMixin,
+  CalendarMultiDayContentTemplateMixin,
+  CalendarTimeLabelTemplateMixin,
+  // components
+  CalendarAgendaInner,
+  CalendarAgendaEvent,
+  CalendarAllDayEvents,
+  CalendarDayColumn,
+  CalendarDayLabels,
+  CalendarEvent,
+  CalendarMonthInner,
+  CalendarMultiDayContent,
+  CalendarTimeLabelColumn
+}
